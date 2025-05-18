247c9c741924a6ec9367b8baa111d4bc3a01847e
Alyssa Ngo <amaengo@protonmail.com>
2025-05-18 14:48:36 +1000

    Started work on academic performance worksheet

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ export default function Home() {
 
   const [lessonArr, setLessonArr] = useState([]);
   const [selectedLesson, setSelectedLesson] = useState("1");
+  const [isAcaPerfWS, setIsAcaPerfWS] = useState(false);
   const [isForKids, setIsForKids] = useState(false);
   const [questions, setQuestions] = useState(0);
   //const [isSimplified, setIsSimplified] = useState(true); 
@@ -21,7 +22,7 @@ export default function Home() {
     fillBlank: false,
     translateChn: false,
     idCorSen: false,
-    reconSentence: false, 
+    reconSentence: false,
   });
 
   const [selectedFormat, setSelectedFormat] = useState("");
@@ -55,13 +56,13 @@ export default function Home() {
 
   const handleFormatChange = (format) => {
     setSelectedFormat(format);
-    
+
     const visibleTypes = new Set(getVisibleQTypes(format));
-    
+
     const updatedQTypes = Object.fromEntries(
       Object.keys(qTypes).map((key) => [key, visibleTypes.has(key) ? qTypes[key] : false])
     );
-  
+
     setQTypes(updatedQTypes);
   };
 
@@ -102,7 +103,7 @@ export default function Home() {
         fill_blank: String(qTypes.fillBlank),
         translate_chn: String(qTypes.translateChn),
         ics: String(qTypes.idCorSen),
-        recon_sentence: String(qTypes.reconSentence), 
+        recon_sentence: String(qTypes.reconSentence),
         question_format: selectedFormat
       },
     })
@@ -138,24 +139,13 @@ export default function Home() {
         <div className="flex flex-col items-center bg-white p-8 rounded-2xl shadow-md w-full max-w-md mt-12">
           <h1 className="text-2xl font-bold mb-4 text-center">Generate Worksheet</h1>
 
-          {/* Level selection */}
-          <section className="flex flex-col items-center mb-4">
-            <h2 className="text-xl font-semibold mb-2">Select Lesson</h2>
-            <select
-              value={selectedLesson}
-              onChange={(e) => setSelectedLesson(e.target.value)}
-              className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              {
-                lessonArr.map((lesson) => (
-                  <option key={lesson.id} value={lesson.id}>{lesson.title}</option>
-                ))
-              }
-            </select>
-          </section>
-
           <section className="flex flex-col items-center mb-4">
             <div className="flex flex-col justify-center gap-2">
+              <label className="flex items-center space-x-2">
+                <input type="checkbox" name="isAcaPerfWS" onChange={() => setIsAcaPerfWS(!isAcaPerfWS)} className="accent-yellow-500" />
+                <span>Generate Academic Performance Worksheet</span>
+              </label>
+
               <label className="flex items-center space-x-2">
                 <input type="checkbox" name="isForKids" onChange={() => setIsForKids(!isForKids)} className="accent-yellow-500" />
                 <span>Are you making this worksheet for kids?</span>
@@ -163,94 +153,118 @@ export default function Home() {
             </div>
           </section>
 
-          {/* Number of questions */}
-          <section className="flex flex-col items-center mb-6">
-            <h2 className="text-xl font-semibold mb-2">Select Number of Questions</h2>
-            <div className="flex gap-4">
-              <label className="flex items-center space-x-2">
-                <input type="radio" name="questions" onChange={() => setQuestions(40)} className="accent-yellow-500" />
-                <span>40</span>
-              </label>
-              <label className="flex items-center space-x-2">
-                <input type="radio" name="questions" onChange={() => setQuestions(50)} className="accent-yellow-500" />
-                <span>50</span>
-              </label>
-              <label className="flex items-center space-x-2">
-                <input type="radio" name="questions" onChange={() => setQuestions(60)} className="accent-yellow-500" />
-                <span>60</span>
-              </label>
-            </div>
-          </section>
-
-          <section className="flex flex-col items-center mb-6">
-            <h2 className="text-xl font-semibold mb-2">Select Question Format</h2>
-            <div className="flex gap-4" onChange={(e) => handleFormatChange(e.target.value)}>
-              <label className="flex items-center space-x-2">
-                <input type="radio" name="qFormat" value="MC" checked={selectedFormat === "MC"} onChange={(e) => setSelectedFormat(e.target.value)} className="accent-yellow-500" />
-                <span>Multiple Choice</span>
-              </label>
-              <label className="flex items-center space-x-2">
-                <input type="radio" name="qFormat" value="WR" checked={selectedFormat === "WR"} onChange={(e) => setSelectedFormat(e.target.value)} className="accent-yellow-500" />
-                <span>Write Answer</span>
-              </label>
-              <label className="flex items-center space-x-2">
-                <input type="radio" name="qFormat" value="MW" checked={selectedFormat === "MW"} onChange={(e) => setSelectedFormat(e.target.value)} className="accent-yellow-500" />
-                <span>Both</span>
-              </label>
-            </div>
-          </section>
-
           {
-            selectedFormat &&
-            <section className="flex flex-col items-center mb-6">
-              <h2 className="text-xl font-semibold mb-2">Select Question Types</h2>
-              <div className="flex flex-col justify-center gap-2">
-                <label className="flex items-center space-x-2">
-                  <input type="checkbox" name="qType" checked={getVisibleQTypes().every(type => qTypes[type])} onChange={handleSelectAllVisible} className="accent-yellow-500" />
-                  <span>Select All</span>
-                </label>
-                <label className="flex items-center space-x-2">
-                  <input type="checkbox" name="qType" checked={qTypes.matchPinyin} onChange={() => setQTypes(prev => ({ ...prev, matchPinyin: !prev.matchPinyin }))} className="accent-yellow-500" />
-                  <span>Match Pinyin to Word</span>
-                </label>
-                <label className="flex items-center space-x-2">
-                  <input type="checkbox" name="qType" checked={qTypes.matchMeaning} onChange={() => setQTypes(prev => ({ ...prev, matchMeaning: !prev.matchMeaning }))} className="accent-yellow-500" />
-                  <span>Match Meaning to Word</span>
-                </label>
-                <label className="flex items-center space-x-2">
-                  <input type="checkbox" name="qType" checked={qTypes.fillBlank} onChange={() => setQTypes(prev => ({ ...prev, fillBlank: !prev.fillBlank }))} className="accent-yellow-500" />
-                  <span>Fill in the Blank</span>
-                </label>
-                {/* Only show certain question types upon reaching a certain lesson or if a question format is selected or both*/}
-                {
-                  selectedFormat !== "MC" &&
-                  <label className="flex items-center space-x-2">
-                    <input type="checkbox" name="qType" checked={qTypes.translateChn} onChange={() => setQTypes(prev => ({ ...prev, translateChn: !prev.translateChn }))} className="accent-yellow-500" />
-                    <span>Translate to Chinese</span>
-                  </label> 
-                }
-                {
-                  selectedLesson >= 4 && selectedFormat !== "WR" &&
-                  <label className="flex items-center space-x-2">
-                    <input type="checkbox" name="qType" checked={qTypes.idCorSen} onChange={() => setQTypes(prev => ({ ...prev, idCorSen: !prev.idCorSen }))} className="accent-yellow-500" />
-                    <span>Identify Correct Sentence</span>
-                  </label>
-                }
-                {
-                  selectedLesson >= 5 && selectedFormat !== "MC" &&
-                  <label className="flex items-center space-x-2">
-                    <input type="checkbox" name="qType" checked={qTypes.reconSentence} onChange={() => setQTypes(prev => ({ ...prev, reconSentence: !prev.reconSentence }))} className="accent-yellow-500" />
-                    <span>Reconstruct Sentence</span>
-                  </label> 
-                }
-              </div>
-            </section>
-          }
+            isAcaPerfWS ? (
+              <></>
+            ) : (
+              <>
+                {/* Level selection */}
+                <section className="flex flex-col items-center mb-4">
+                  <h2 className="text-xl font-semibold mb-2">Select Lesson</h2>
+                  <select
+                    value={selectedLesson}
+                    onChange={(e) => setSelectedLesson(e.target.value)}
+                    className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  >
+                    {
+                      lessonArr.map((lesson) => (
+                        <option key={lesson.id} value={lesson.id}>{lesson.title}</option>
+                      ))
+                    }
+                  </select>
+                </section>
 
-          {/* Submit button */}
-          <button disabled={questions === 0 || selectedFormat === "" || !Object.values(qTypes).some(value => value === true)} onClick={() => handleGenerate()} className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed">
-            Generate
-          </button>
+                {/* Number of questions */}
+                <section className="flex flex-col items-center mb-6">
+                  <h2 className="text-xl font-semibold mb-2">Select Number of Questions</h2>
+                  <div className="flex gap-4">
+                    <label className="flex items-center space-x-2">
+                      <input type="radio" name="questions" onChange={() => setQuestions(40)} className="accent-yellow-500" />
+                      <span>40</span>
+                    </label>
+                    <label className="flex items-center space-x-2">
+                      <input type="radio" name="questions" onChange={() => setQuestions(50)} className="accent-yellow-500" />
+                      <span>50</span>
+                    </label>
+                    <label className="flex items-center space-x-2">
+                      <input type="radio" name="questions" onChange={() => setQuestions(60)} className="accent-yellow-500" />
+                      <span>60</span>
+                    </label>
+                  </div>
+                </section>
+
+                <section className="flex flex-col items-center mb-6">
+                  <h2 className="text-xl font-semibold mb-2">Select Question Format</h2>
+                  <div className="flex gap-4" onChange={(e) => handleFormatChange(e.target.value)}>
+                    <label className="flex items-center space-x-2">
+                      <input type="radio" name="qFormat" value="MC" checked={selectedFormat === "MC"} onChange={(e) => setSelectedFormat(e.target.value)} className="accent-yellow-500" />
+                      <span>Multiple Choice</span>
+                    </label>
+                    <label className="flex items-center space-x-2">
+                      <input type="radio" name="qFormat" value="WR" checked={selectedFormat === "WR"} onChange={(e) => setSelectedFormat(e.target.value)} className="accent-yellow-500" />
+                      <span>Write Answer</span>
+                    </label>
+                    <label className="flex items-center space-x-2">
+                      <input type="radio" name="qFormat" value="MW" checked={selectedFormat === "MW"} onChange={(e) => setSelectedFormat(e.target.value)} className="accent-yellow-500" />
+                      <span>Both</span>
+                    </label>
+                  </div>
+                </section>
+
+                {
+                  selectedFormat &&
+                  <section className="flex flex-col items-center mb-6">
+                    <h2 className="text-xl font-semibold mb-2">Select Question Types</h2>
+                    <div className="flex flex-col justify-center gap-2">
+                      <label className="flex items-center space-x-2">
+                        <input type="checkbox" name="qType" checked={getVisibleQTypes().every(type => qTypes[type])} onChange={handleSelectAllVisible} className="accent-yellow-500" />
+                        <span>Select All</span>
+                      </label>
+                      <label className="flex items-center space-x-2">
+                        <input type="checkbox" name="qType" checked={qTypes.matchPinyin} onChange={() => setQTypes(prev => ({ ...prev, matchPinyin: !prev.matchPinyin }))} className="accent-yellow-500" />
+                        <span>Match Pinyin to Word</span>
+                      </label>
+                      <label className="flex items-center space-x-2">
+                        <input type="checkbox" name="qType" checked={qTypes.matchMeaning} onChange={() => setQTypes(prev => ({ ...prev, matchMeaning: !prev.matchMeaning }))} className="accent-yellow-500" />
+                        <span>Match Meaning to Word</span>
+                      </label>
+                      <label className="flex items-center space-x-2">
+                        <input type="checkbox" name="qType" checked={qTypes.fillBlank} onChange={() => setQTypes(prev => ({ ...prev, fillBlank: !prev.fillBlank }))} className="accent-yellow-500" />
+                        <span>Fill in the Blank</span>
+                      </label>
+                      {/* Only show certain question types upon reaching a certain lesson or if a question format is selected or both*/}
+                      {
+                        selectedFormat !== "MC" &&
+                        <label className="flex items-center space-x-2">
+                          <input type="checkbox" name="qType" checked={qTypes.translateChn} onChange={() => setQTypes(prev => ({ ...prev, translateChn: !prev.translateChn }))} className="accent-yellow-500" />
+                          <span>Translate to Chinese</span>
+                        </label>
+                      }
+                      {
+                        selectedLesson >= 4 && selectedFormat !== "WR" &&
+                        <label className="flex items-center space-x-2">
+                          <input type="checkbox" name="qType" checked={qTypes.idCorSen} onChange={() => setQTypes(prev => ({ ...prev, idCorSen: !prev.idCorSen }))} className="accent-yellow-500" />
+                          <span>Identify Correct Sentence</span>
+                        </label>
+                      }
+                      {
+                        selectedLesson >= 5 && selectedFormat !== "MC" &&
+                        <label className="flex items-center space-x-2">
+                          <input type="checkbox" name="qType" checked={qTypes.reconSentence} onChange={() => setQTypes(prev => ({ ...prev, reconSentence: !prev.reconSentence }))} className="accent-yellow-500" />
+                          <span>Reconstruct Sentence</span>
+                        </label>
+                      }
+                    </div>
+                  </section>
+                }
+                
+                {/* Submit button */}
+                <button disabled={questions === 0 || selectedFormat === "" || !Object.values(qTypes).some(value => value === true)} onClick={() => handleGenerate()} className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 cursor-pointer disabled:bg-gray-400 disabled:cursor-not-allowed">
+                  Generate
+                </button>
+              </>
+            )
+          }
         </div>
       </div>
     </main>
